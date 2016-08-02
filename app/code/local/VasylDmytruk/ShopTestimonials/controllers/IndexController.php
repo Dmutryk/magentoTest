0b81c786aa2525a47236aa575c829b21525906d7
Sladar <v.dmytruk@bvblogic.com>
2016-08-02 11:59:59 +0300

    Initial commit

---
+++ app/code/local/VasylDmytruk/ShopTestimonials/controllers/IndexController.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sladar
+ * Date: 01.08.2016
+ * Time: 17:47
+ */
+class VasylDmytruk_ShopTestimonials_IndexController extends Mage_Core_Controller_Front_Action{
+    public function indexAction(){
+        echo "Just TEST =)";
+    }
+}
